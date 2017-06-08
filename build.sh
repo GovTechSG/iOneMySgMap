7733a4b1e91d6a309c55a31618694d9792e1f637
Yong Wen Chua <me@yongwen.xyz>
2017-06-08 16:03:35 +0800

    Add config

--- build.sh
+++ build.sh
@@ -7,4 +7,6 @@ mkdir -p data
 echo "====> : Downloading Singapore Metro Extract"
 curl https://s3.amazonaws.com/metro-extracts.mapzen.com/singapore.osm.pbf -o data/singapore.osm.pbf
 
+cp ../docker-compose-config.yml data/docker-compose-config.yml
+
 ./quickstart.sh singapore
