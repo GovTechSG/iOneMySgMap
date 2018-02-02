4f7e5027aba165626be26233375653ed9774be8d
Chris Sng <chrissng@users.noreply.github.com>
2018-02-02 13:44:33 +0800

    use env file everywhere (#3)
    - as documented in https://github.com/openmaptiles/openmaptiles/blob/f591f2e28efd377f529cfbae3a89e55c0d0bff2c/QUICKSTART.md#change-min_zoom-and-max_zoom
    - empty docker-compose-config.yml

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ mkdir -p data
 echo "====> : Downloading Singapore Metro Extract"
 curl https://s3.amazonaws.com/metro-extracts.mapzen.com/singapore.osm.pbf -o data/singapore.osm.pbf
 
+cp ../.env .env
 cp ../docker-compose-config.yml data/docker-compose-config.yml
 
 ./quickstart.sh singapore
