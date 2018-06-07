506607a8c691ce2163018acccba622065b09b3dc
Chris Sng <chrissng@users.noreply.github.com>
2018-06-07 20:32:25 +0800

    Use Geofabrick for raw OSM data (#5)
    - This is because of Mapzen's shutdown which ended Metro Extracts data service

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd openmaptiles
 mkdir -p data
 
 echo "====> : Downloading Singapore Metro Extract"
-curl https://s3.amazonaws.com/metro-extracts.mapzen.com/singapore.osm.pbf -o data/singapore.osm.pbf
+curl https://download.geofabrik.de/asia/malaysia-singapore-brunei-latest.osm.pbf -o data/singapore.osm.pbf
 
 cp ../.env .env
 cp ../docker-compose-config.yml data/docker-compose-config.yml
